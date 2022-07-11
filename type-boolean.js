44d0c1482bf41935e93483faff1a2244cc9ba034
ericksposito <ericksposito@gmail.com>
2022-07-11 10:43:28 -0300

    exemplos aula 1

---
+++ type-boolean.js
@@ -0,0 +1,15 @@
+//boolean
+//false -> falso
+//true -> verdadeiro
+
+// const primeiroNumero = 5;
+// const segundoNumero = 10;
+// let cadastroAtivado = true;
+
+// console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2);
+
---
+++ type-number.js
@@ -0,0 +1,21 @@
+//tipo Number
+
+const meuNumero = 3
+const primeiroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = primeiroNumero - segundoNumero
+
+console.log(operacaoMatematica)
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3
+const pontoFlutuanteSemZero = 0
+const novaOperacao = primeiroNumero / pontoFlutuanteSemZero
+
+console.log(novaOperacao)
+
+// NaN -> Not A Number (Não é um número)
+const alura = 'Alura'
+console.log(alura * primeiroNumero)
