9c1ed1be4447273b75e653129cbf8089dd6e7557
ericksposito <ericksposito@gmail.com>
2022-07-11 11:16:24 -0300

    cod var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,21 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const numero = 456
+const numeroString = '456'
+
+const numeroStringComA = '456a'
+
+console.log(numero == numeroString)
+
+console.log(numero === numeroString)
+
+console.log(numero + numeroString)
+
+//Number()
+//String()
+console.log(numero + Number(numeroString))
+console.log(numero + Number(numeroStringComA))
+
+// conversão explicita
