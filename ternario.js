4d03ada108948fc2773606b38afcd7c8ae18322e
ericksposito <ericksposito@gmail.com>
2022-07-11 12:10:14 -0300

    comparacao template e ternario

---
+++ ternario.js
@@ -0,0 +1,12 @@
+const idadeMinima = 18
+const idadeCliente = 20
+
+if (idadeCliente >= idadeMinima) {
+  console.log('Cerveja')
+} else {
+  console.log('Suco')
+}
+
+//ternario
+console.log(idadeCliente >= idadeMinima ? 'cerveja' : 'suco')
+              //condicao                //true      //false
