3486c252ad93760f00825a0e54876a8c549de50f
ericksposito <ericksposito@gmail.com>
2022-07-12 08:58:18 -0300

    funcoes parametros arrow etc

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) declara a função
+            //string
+function imprimeTexto(texto) {
+  console.log(texto)
+}
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto(soma());
+imprimeTexto("outro texto");
+
+// três formas de escrever função
+
+function soma() {
+  //outros códigos
+  //varios console.log()
+  return 2 + 2;
+}
+
+// console.log(soma());
